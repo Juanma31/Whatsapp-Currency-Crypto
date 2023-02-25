5f5267aa9156fcec05b7cb6e5f61427a1aeb7950
Juanma31 <65458652+Juanma31@users.noreply.github.com>
2023-02-25 18:21:01 +0000

    Update twilio_whatsapp_script.py

--- twilio_whatsapp_script.py
+++ twilio_whatsapp_script.py
@@ -1,15 +1,15 @@
 """
 ************************************************************************
-* Author = @Juanma31                                                   *
-* Date = '22/02/2023'                                                  *
-* Description = Sending whatsapp messages using Python. Currency       *
+* Author = @Juanma31                                                             *
+* Date = '25/02/2023'                                                            *
+* Description = Sending whatsapp messages using Python. Currency & Crypto.       *
 ************************************************************************
 """
 
 
 import os
 from twilio.rest import Client
-from twilio_Configg import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,WHATSAPP_NUMBER,API_KEY_CURR
+from twilio_Configg import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,WHATSAPP_NUMBER,API_KEY_CURR,API_KEY_CRYP
 import time
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
@@ -26,15 +26,27 @@ base_currency= 'GBP'
 currency1='EUR'
 currency2='USD'
 api_key = API_KEY_CURR
+api_crypto=API_KEY_CRYP
 
 input_date= get_date()
+
+#Currency
 response = request_capi(api_key,currency1,currency2,base_currency)
 
 query=get_currency(response)
 
+#Cryptos
+data=request_crypapi(api_crypto)
+data_crypto = []
+
+for i in tqdm(range(10),colour = 'green'):
+    
+    data_crypto.append(get_cryptos(data,i))
+    
+df_crypto=create_df_crypto(data_crypto)
 
 # Send Message
-message_id = send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,query)
+message_id = send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,query,df_crypto)
 
 
-print('Mensaje Enviado con exito ' + message_id)
+print('Message sent succesfully ' + message_id)
