e3a0be97c0e266685f12747a4776e79ac5704a92
Juanma31 <65458652+Juanma31@users.noreply.github.com>
2023-02-25 19:11:51 +0000

    Update twilio_whatsapp_script.py

--- twilio_whatsapp_script.py
+++ twilio_whatsapp_script.py
@@ -9,7 +9,7 @@
 
 import os
 from twilio.rest import Client
-from twilio_Configg import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,WHATSAPP_NUMBER,API_KEY_CURR,API_KEY_CRYP
+from twilio_Configg import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,WHATSAPP_NUMBER,API_KEY_CURR,API_KEY_CRYP,MY_WHATSAPP_NUMBER
 import time
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
