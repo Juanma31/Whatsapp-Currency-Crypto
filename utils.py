23c8248bac33f3388a51c69b596fd7ff2698325a
Juanma31 <65458652+Juanma31@users.noreply.github.com>
2023-02-25 18:21:48 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
-
 import pandas as pd
 from twilio.rest import Client
-from twilio_Configg import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,WHATSAPP_NUMBER,API_KEY_CURR
+from twilio_Configg import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,WHATSAPP_NUMBER,API_KEY_CURR,API_KEY_CRYP
 from datetime import datetime
 import requests
 from requests import Request, Session
@@ -34,7 +33,44 @@ def get_currency(response):
     
     return currency_eur,currency_usd
 
-def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,query):
+def request_crypapi(api_crypto)
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+    parameters = {
+      'start':'1',
+      'limit':'1000',
+      'convert':'USD'
+    }
+    headers = {
+      'Accepts': 'application/json',
+      'X-CMC_PRO_API_KEY': api_crypto,
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+      response = session.get(url, params=parameters)
+      data = json.loads(response.text)
+      print(data)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+      print(e)
+    
+def get_cryptos(data,i):
+    #name=data['data'][i]['name']
+    symbol=data['data'][i]['symbol']
+    price=data['data'][i]['quote']['USD']['price']
+    rank=data['data'][i]['cmc_rank']
+    
+    return symbol,price,rank
+   
+def create_df_crypto(data_crypto):
+    col=['Symbol','Price','Rank']
+    df=pd.DataFrame(data_crypto,columns=col)
+    df=df.sort_values(by='Rank',ascending=True)
+    return df   
+    
+def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,query,df):
 
     account_sid = TWILIO_ACCOUNT_SID
     auth_token = TWILIO_AUTH_TOKEN
@@ -43,7 +79,7 @@ def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,query):
 
     message = client.messages \
                     .create(
-                        body='\nHello! \n\n\n Today, '+input_date+', the GBP is equal to '+ str(query[0]) + ' EUR and '+ str(query[1])+' AUD',
+                        body='\nHello! \n\n\n Today, '+input_date+', the GBP is equal to '+ str(query[0]) + ' EUR and '+ str(query[1])+' AUD. And here is the summary of cryptos in USD: \n'str(df),
                         from_=WHATSAPP_NUMBER,
                         to='whatsapp:+447568279452'
                     )
